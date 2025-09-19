decf697b1f8008d1babececd2b73c783fbcf9dc7
IrinaG234 <irinaglodin@gmail.com>
2025-09-19 19:34:47 +0300

    again->Settings page(Admin)

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,10 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/lib/generated/prisma";
 
 declare global {
+  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
-};
+}
 
-const prismadb = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
+const prismadb = global.prisma || new PrismaClient({
+  log: ["query", "error", "warn"],
+});
+
+if (process.env.NODE_ENV !== "production") global.prisma = prismadb;
 
 export default prismadb;
+
